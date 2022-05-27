0bbe7d6726774ccf678fd2a0d1e5b6464f6e05d0
V. Sai Prakash <vukkumsp@gmail.com>
2022-05-27 22:28:57 +0530

    done some basic features;

--- project-2/src/App.js
+++ project-2/src/App.js
@@ -2,9 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const myFunction = () => {
+    alert("Alert Popped !!!");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +22,18 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <div className="customClass">
+        <div className="card" style={{ width: "18rem" }}>
+          <img src={logo} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="https://www.w3schools.com/react/react_css.asp" className="btn btn-primary">Go somewhere</a>
+            <button onClick={myFunction} className="btn btn-primary">Take the shot!</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
